19602b6c79adaf97447c67b1faa744748d6367d6
Mehul <mehulrastogi2002@gmail.com>
2024-11-03 16:18:59 -0500

    initial commits

---
+++ code/mobility_model.py
@@ -0,0 +1,18 @@
+from source import compile_data
+
+def clean_data():
+    data = compile_data()
+    shrink_data = data.groupby(["year", "week"]).agg({
+        'new_cases' : 'sum',
+        'new_deaths' : 'sum',
+        'new_vaccinations': 'sum',
+        'year': 'first',
+        'week': 'first',
+        'avg_USA': 'first'
+    })
+    shrink_data = shrink_data.rename(columns={'avg_USA': 'mobility_data'})
+    shrink_data.to_csv('../data/weekly_cleaned_data.csv', index=False)
+    return shrink_data
+
+if __name__ == "__main__":
+    data = clean_data()
