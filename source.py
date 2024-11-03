308af92463c825c538d73b08264ab3aef6a8bce3
akarwa7 <akshatkarwa@gatech.edu>
2024-11-03 14:35:08 -0500

    added proposal and started data cleaning

--- source.py
+++ source.py
@@ -4,5 +4,17 @@ def read_data(filePath):
     data = pd.read_csv(filePath)
     return data
 
-filePath = "cowid-covid-data.csv"
-data = read_data(filePath)
+def get_USA_data(data):
+    return data[data['iso_code'] == 'USA']
+
+def remove_columns(data):
+    columns_to_drop = ['iso_code', 'continent']
+    data = data.drop(columns_to_drop, axis=1)
+    return data
+
+if __name__ == "__main__":
+    filePath = "cowid-covid-data.csv"
+    data = read_data(filePath)
+    US_data = get_USA_data(data)
+    filtered_data = remove_columns(US_data)
+    print(filtered_data)
