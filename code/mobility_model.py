ed60830d4a8249ea56f76336ed685d57445bfa3f
Mehul <mehulrastogi2002@gmail.com>
2024-11-03 16:48:05 -0500

    graphs added

--- code/mobility_model.py
+++ code/mobility_model.py
@@ -1,4 +1,5 @@
 from source import compile_data
+import matplotlib.pyplot as plt
 
 def clean_data():
     data = compile_data()
@@ -15,5 +16,36 @@ def clean_data():
     shrink_data.to_csv('../data/weekly_cleaned_data.csv', index=False)
     return shrink_data
 
+def plot_data(data, parameter):
+    print_parameter = None
+    if parameter == "new_cases":
+        print_parameter = "New Cases"
+    elif parameter == "new_deaths":
+        print_parameter = "New Deaths"
+    
+    for year in [2020, 2021, 2022]:
+        current_year_data = data[data['year'] == year]
+        
+        _, axis_left = plt.subplots(figsize=(12, 6))
+
+        axis_left.plot(current_year_data["week"], current_year_data["mobility_data"], label="Mobility Data", color = "green")
+        axis_left.set_ylabel("Mobility Data", color = "green")
+        axis_left.set_title("Weekly Mobility and " + str(print_parameter) + "data for " + str(year))
+
+        axis_right = axis_left.twinx()
+        axis_right.plot(current_year_data['week'], current_year_data[parameter], label=print_parameter, color = "red")
+        axis_right.set_ylabel(print_parameter, color = "red")
+
+        axis_left.grid(True)
+        axis_left.set_xlabel("Week")
+
+        plt.savefig("../plots/mobility/" + str(year) + "-" + str(parameter) + "-mobility-plot.png", format = "png")
+        plt.show()
+
+def graph_plots(data):
+    plot_data(data, "new_cases")
+    plot_data(data, "new_deaths")
+
 if __name__ == "__main__":
     data = clean_data()
+    graph_plots(data)
